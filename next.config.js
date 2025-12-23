04f8e822b336b52ef7e53b104540a86a5e92322e
oyunsanaachatbot-create <oyunsanaachatbot@gmail.com>
2025-12-24 02:30:18 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  reactStrictMode: false,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-  reactStrictMode: false, // changed this to false
-  images: {
-    domains: [
-      'images.unsplash.com',
-      'i.ibb.co',
-      'scontent.fotp8-1.fna.fbcdn.net',
-    ],
-    // Make ENV
-    unoptimized: true,
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
