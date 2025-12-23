9613fa69bedb40404f0a728dce7a2d1087dfe8bb
oyunsanaachatbot-create <oyunsanaachatbot@gmail.com>
2025-12-24 01:41:09 +0800

    Update Avatar.tsx

--- src/components/image/Avatar.tsx
+++ src/components/image/Avatar.tsx
@@ -1,38 +1,43 @@
-'use client'
+'use client';
+
 import { chakra, useColorMode } from '@chakra-ui/system';
 import { ComponentProps } from 'react';
 import { Image } from './Image';
 
 type AvatarImageProps = Partial<
-    ComponentProps<typeof Image> & {
-        showBorder?: boolean;
-    }
+  ComponentProps<typeof Image> & {
+    showBorder?: boolean;
+  }
 >;
 
 export function NextAvatar({
-    src,
-    showBorder,
-    alt = '',
-    style,
-    ...props
+  src,
+  showBorder,
+  alt = '',
+  style,
+  ...props
 }: AvatarImageProps) {
-    const { colorMode } = useColorMode();
+  const { colorMode } = useColorMode();
 
-    return (
-        <Image
-            {...props}
-            {...(showBorder
-                ? {
-                      border: '2px',
-                      borderColor: colorMode === 'dark' ? 'navy.700' : 'white',
-                  }
-                : {})}
-            alt={alt}
-            objectFit={'fill'}
-            src={src}
-            style={{ ...style, borderRadius: '50%' }}
-        />
-    );
+  return (
+    <Image
+      {...props}
+      {...(showBorder
+        ? {
+            border: '2px',
+            borderColor: colorMode === 'dark' ? 'navy.700' : 'white',
+          }
+        : {})}
+      alt={alt}
+      src={src}
+      // ✅ objectFit prop-гүй
+      style={{
+        borderRadius: '50%',
+        objectFit: 'cover', // эсвэл 'fill' хэрэгтэй бол 'fill' гэж тавь
+        ...style, // хэрэглэгчийн style override хийх боломжтой
+      }}
+    />
+  );
 }
 
 export const ChakraNextAvatar = chakra(NextAvatar, {
