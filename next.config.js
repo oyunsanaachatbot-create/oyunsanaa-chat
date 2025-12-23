9536e94138165c3ab340f19a8ad55d3d651f609f
oyunsanaachatbot-create <oyunsanaachatbot@gmail.com>
2025-12-24 02:31:54 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
+
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'i.ibb.co',
+      'scontent.fotp8-1.fna.fbcdn.net',
+    ],
+    unoptimized: true,
+  },
+
+  // ✅ Build дээр ESLint rule crash-ийг тойрно (код чинь буруу биш, plugin зөрөөд байгаа)
   eslint: {
     ignoreDuringBuilds: true,
   },
