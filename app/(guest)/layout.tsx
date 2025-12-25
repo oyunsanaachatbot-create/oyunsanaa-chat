eb2d1b22e98a1f14ae709c2134b9d57e4daca0eb
oyunsanaachatbot-create <oyunsanaachatbot@gmail.com>
2025-12-25 21:02:05 +0800

    Delete app/(guest) directory

--- app/(guest)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import DashboardShell from '../(dashboard)/DashboardShell';
-
-export default function GuestLayout({ children }: { children: React.ReactNode }) {
-  return <DashboardShell>{children}</DashboardShell>;
-}
