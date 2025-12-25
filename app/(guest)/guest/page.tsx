8d9afffad3e8df1d7e70cf33bc43024177adf554
oyunsanaachatbot-create <oyunsanaachatbot@gmail.com>
2025-12-25 20:35:23 +0800

    Update page.tsx

--- app/(guest)/guest/page.tsx
+++ app/(guest)/guest/page.tsx
@@ -1,1 +1,8 @@
-export { default } from '../../(dashboard)/chat/page';
+// app/(guest)/guest/page.tsx
+'use client';
+
+import ChatShell from '@/components/ChatShell';
+
+export default function GuestPage() {
+  return <ChatShell />;
+}
