f427d96ad15e599bebc406632a8da3a596d62b37
oyunsanaachatbot-create <oyunsanaachatbot@gmail.com>
2025-12-25 21:30:04 +0800

    Create page.tsx

---
+++ app/guest/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+export default function GuestPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h2>Guest Chat</h2>
+      <p>Энд чинь login шаардахгүй туршилтын chat UI чинь байна.</p>
+
+      {/* ✅ ЭНД өөрийнхөө жинхэнэ Chat UI component-оо тавина */}
+      {/* <ChatUI mode="guest" /> */}
+    </div>
+  );
+}
